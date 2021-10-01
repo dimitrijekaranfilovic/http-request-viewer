2bb73e381d4e6a1dea53b3f0a53bd2e61569e3da
Dimitrije Karanfilovic <dimitrije.karanfilovic@uns.ac.rs>
2021-10-01 20:30:28 +0200

    Added additional elements.

--- src/components/FormRequest.js
+++ src/components/FormRequest.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormRequest = () => {
-  return <div>FETCH/XMLHTTP</div>;
+  const [api, setApi] = useState("fetch");
+
+  return (
+    <React.Fragment>
+      <div>
+        <h4>FETCH/XMLHTTP</h4>
+        <span>
+          <label htmlFor="api-select">API</label>
+          <select
+            name="api-select"
+            id="api-select"
+            value={api}
+            onChange={(e) => setApi(e.target.value)}
+          >
+            <option value="fetch">Fetch</option>
+            <option value="xml">XMLHtp</option>
+          </select>
+        </span>
+      </div>
+
+      <textarea name="area" id="area" cols="50" rows="15"></textarea>
+      <div>
+        <button type="button">Perform</button>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default FormRequest;
--- src/components/Response.js
+++ src/components/Response.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Response = () => {
-  return <div>RESPONSE</div>;
+  return (
+    <React.Fragment>
+      <div className="response-div">
+        <h4>Response</h4>
+        <textarea
+          name="response"
+          id="response"
+          cols="50"
+          rows="15"
+          readOnly
+        ></textarea>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Response;
